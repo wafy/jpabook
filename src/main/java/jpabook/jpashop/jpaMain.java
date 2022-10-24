9bfb94f9d440d4dfbd2220d40712724bba4649e9
최선혁 <wafy75@gmail.com>
2022-10-24 17:53:18 +0900

    엔티티 생성

---
+++ src/main/java/jpabook/jpashop/jpaMain.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class jpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+        } catch (Exception e) {
+            em.close();
+        } finally {
+            emf.close();
+        }
+    }
+}
